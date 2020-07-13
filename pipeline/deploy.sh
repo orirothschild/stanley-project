adb4021d99e9239b05aa706f703967982a5675f5
EC2 Default User <ec2-user@ip-172-31-54-206.ec2.internal>
2020-07-13 11:21:01 +0000

    new push

--- pipeline/deploy.sh
+++ pipeline/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "****************************"
-echo "*****deploy new image******"
+echo "*****deploy new image*******"
 echo "****************************"
 docker run -d -p 8080:8080 orirothschild/stanley-project:latest
