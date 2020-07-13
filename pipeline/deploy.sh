81455b9587b5d6430ce2f19a3ad32701f29daaf8
EC2 Default User <ec2-user@ip-172-31-54-206.ec2.internal>
2020-07-13 11:16:32 +0000

    file should now run

--- pipeline/deploy.sh
+++ pipeline/deploy.sh
@@ -2,4 +2,4 @@
 echo "****************************"
 echo "*****deploy new image******"
 echo "****************************"
-docker run -d -p 8090:8080 orirothschild/stanley-project:latest
+docker run -d -p 8080:8080 orirothschild/stanley-project:latest
